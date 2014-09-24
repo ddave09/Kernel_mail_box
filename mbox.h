765f514213715f2c7706e11b9acac33f2ba1207b
Darshit Dave <dd1691@nyu.edu>
2014-09-24 00:58:22 -0400

    Create mbox.h

---
+++ mbox.h
@@ -0,0 +1,38 @@
+#ifndef __mbox
+#define __mbox
+#define MAX_MAILBOX 256
+
+extern unsigned long existing_mailboxes;
+
+struct mailbox_listing{
+	unsigned char exists;
+	struct mailbox *mailbox_ptr;
+} extern mailbox_list[256];
+
+
+struct message{
+	unsigned long size;
+	char *msg;
+	struct message *next;
+};
+
+struct mailbox{
+	unsigned long existing_messages;
+	unsigned long mxid;
+	struct message *msg;
+	struct message *last_node;
+};
+
+/* --------------------------------------- Helper functions declaration ------------------------------------------- */
+
+long free_messages(struct message *msg);
+
+/* ---------------------------------------------------------------------------------------------------------------- */
+#endif
+
+
+
+
+
+
+
